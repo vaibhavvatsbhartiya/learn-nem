2aabb9118eeca3df600ff9daddc3253f69a2c3c6
Vaibhav vats <vabhavvats30@gmail.com>
2023-12-24 14:13:59 +0530

    1. Modules related chit-chat

---
+++ index.js
@@ -0,0 +1,26 @@
+// pahle use hota tha 
+// const lib = require('./lib');
+
+
+//  but ab
+import { sum, diff } from "./lib.js";
+import * as fs from 'node:fs';
+
+
+
+// const txt = fs.readFileSync('demo.txt', "Utf-8"); // read our file but have to specify the format
+// console.log(txt);
+// but the problem is k yeh bhout time consuming hai if file bhout bde hai ya server ko request kr rahe hsi data k liye 
+
+
+// to solve this problem we need to use callbacks or promises 
+// here i use callback 
+fs.readFile('demo.txt', "Utf-8", (err, data) =>{
+    console.log(data);
+});
+
+console.log(sum(2,3),"and", diff(5,3));
+
+
+// make sure to always use async code to save as much time as u can 
+// because ik sync code bhout time consuming hota hai.
---
+++ lib.js
@@ -0,0 +1,10 @@
+// Modules bn giye hai yeh
+// we have to export each module if we need to use them in other files 
+
+export const sum = (a,b) =>{
+    return a + b ;
+}
+
+export const diff = (a,b) =>{
+    return a - b ;
+}
