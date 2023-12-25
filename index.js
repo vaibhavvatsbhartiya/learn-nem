878afe20fad3e5585a1914c73b0526953c0d92b0
Vaibhav vats <vabhavvats30@gmail.com>
2023-12-25 11:24:10 +0530

    kooch error show kr rha hai inko smz and solve krna hai.

--- index.js
+++ index.js
@@ -3,21 +3,65 @@ import * as fs from "node:fs";
 
 const index = fs.readFileSync("index.html", "Utf-8");
 
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
+const products = data.products;
+
+
+
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" }); // Header ko define kr ja rha hai
-  // Node bhout dumb hai so we use "JSON.stringify" method
-  // To set data as string otherwise vo error d deta apne ko
+  console.log(req.url, req.method);
 
-  //   res.end(JSON.stringify({
-  //     data: 'Hello World!',
-  //   }));
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(index);
+
+  if(req.url.startsWith('/product')){
+    const id = req.url.split('/')[2]
+    const product = products.find(p=>p.id===(+id)) // yeh higher order function yaha use kra ja raha hai
+    console.log(product)
+    res.setHeader('Content-Type', 'text/html');
+          let modifiedIndex = index.replace('**title**', product.title)
+          .replace('**url**', product.thumbnail)
+          .replace('**price**', product.price)
+          .replace('**rating**', product.rating)
+          res.end(modifiedIndex);
+          return;
+  }
+
+
+//   '/product':
+//       res.setHeader('Content-Type', 'text/html');
+//       let modifiedIndex = index.replace('**title**', product.title)
+//       .replace('**url**', product.thumbnail)
+//       .replace('**price**', product.price)
+//       .replace('**rating**', product.rating)
+//       res.end(modifiedIndex);
+//       break;
+
+
+
+switch (req.url) {
+  case '/':
+    res.setHeader('Content-Type', 'text/html');
+    res.end(index);
+    break;
+  case '/api':
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(data));
+    break;
+
+  default:
+    res.writeHead(404);
+    res.end();
+}
+
+console.log('server started  ');
+//   res.setHeader('Dummy', 'DummyValue');
+
+//
 });
 
 server.listen(8000);
 
 
-// Reference
-// https://nodejs.org/docs/latest/api/http.html#httpcreateserveroptions-requestlistener
+//  Hala ki mane code copy kr k use krne ka try kra but ik baat toh smz m aa giyee k 
+// node js k help s server create krna complex hai bhout
